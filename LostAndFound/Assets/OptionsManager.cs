0a619701c4a504c0bd5409367300e34a5816a193
Underfide0 <ulisesgedo@gmail.com>
2024-05-07 20:32:01 +0200

    MenuAcabado

--- LostAndFound/Assets/OptionsManager.cs
+++ LostAndFound/Assets/OptionsManager.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Slider musicSlider, sfxSlider, brightnessSlider;
     void Start()
     {
-        Screen.SetResolution(640, 480, true);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void Awake()
+    {
+      
+        DontDestroyOnLoad(gameObject);
+       
+    }
+
+    public void MusicVolume()
+    {
+        AudioManager.instance.MusicVolume(musicSlider.value);
+    }
+
+    public void SFXVolume()
+    {
+        AudioManager.instance.SFXVolume(sfxSlider.value);
+    }
+    public void brightnessAmount()
+    {
+        Screen.brightness = brightnessSlider.value;
+    }
+
+    public void resolutionDropdown(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                Screen.SetResolution(1920, 1080, true);
+                break;
+            case 1:
+                Screen.SetResolution(1280, 800, true);
+                break;
+            case 2:
+                Screen.SetResolution(640, 480, true);
+                break;
+        }
     }
 }
