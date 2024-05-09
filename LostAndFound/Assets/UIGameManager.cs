337ef9c10c2532cec3a0d8177f6b5f7eb399d0bf
Underfide0 <ulisesgedo@gmail.com>
2024-05-09 09:22:25 +0200

    ControllerChanges

--- LostAndFound/Assets/UIGameManager.cs
+++ LostAndFound/Assets/UIGameManager.cs
@@ -2,14 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class UIGameManager : MonoBehaviour
 {
     [SerializeField] private Image Stamina;
-    
+
+    [SerializeField] private GameObject controllerInteractUI;
+
+    [SerializeField] private GameObject keyboardInteractUI;
+
     void Start()
     {
         UpdateStamina(100);
+
+        InputSystem.onDeviceChange += (device, change) =>
+        {
+            switch (change)
+            {
+                case InputDeviceChange.Added:
+                    keyboardInteractUI.SetActive(false);
+                    controllerInteractUI.SetActive(true);
+                    Debug.Log("Mando conectado");
+                    break;
+                case InputDeviceChange.Removed:
+                    keyboardInteractUI.SetActive(true);
+                    controllerInteractUI.SetActive(false);
+                    Debug.Log("Mando desconectado");
+                    break;
+            }
+        };
     }
 
     // Update is called once per frame
@@ -26,4 +48,5 @@ public class UIGameManager : MonoBehaviour
     {
         Stamina.fillAmount = currentStamina/100;
     }
+
 }
