57e00a0d6cc3dcab4206b7d0cd6de6aee3ac923f
Underfide0 <ulisesgedo@gmail.com>
2024-05-10 09:45:03 +0200

    UICardboardArreglada

--- LostAndFound/Assets/Scripts/ManagerScripts/PhotoManager.cs
+++ LostAndFound/Assets/Scripts/ManagerScripts/PhotoManager.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhotoManager : MonoBehaviour
 {
     public Material[] materialToApply;
+
+    public RawImage[] images; 
 
     public int numberPhoto;
 
@@ -22,49 +25,6 @@ public class PhotoManager : MonoBehaviour
 
     // Update is called once per frame
     
-
-    public void ScreenshotOld()
-    {
-
-        int numPhotos = materialToApply.Length;
-
-        for (int i = 0; i < numPhotos; i++)
-        {
-            Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-            texture.ReadPixels(new Rect(2, 2, Screen.width, Screen.height), 2, 2);
-            texture.Apply();
-
-            photosTaken++;
-
-            byte[] bytes = texture.EncodeToPNG();
-            string filePath = Path.Combine(Application.persistentDataPath, "screenshot_" + i + ".png");
-            File.WriteAllBytes(filePath, bytes);
-
-            Destroy(texture);
-
-            Debug.Log("Screenshot " + i + " saved on " + filePath);
-
-            if (i < materialToApply.Length)
-            {
-                Texture2D tex = new Texture2D(2, 2);
-                tex.LoadImage(bytes);
-                materialToApply[numberPhoto].mainTexture = tex;
-            }
-            else
-            {
-                Debug.LogWarning("Algo has hecho mal");
-                break;
-            }
-        }
-
-        if (photosTaken == 10)
-        {
-            comeBackUI.SetActive(true);
-        }
-
-        Debug.Log("phototaken");
-    }
-
     public void Screenshot()
     {
         
@@ -78,8 +38,6 @@ public class PhotoManager : MonoBehaviour
             string filePath = Path.Combine(Application.persistentDataPath, "screenshot_" + numberPhoto + ".png");
             File.WriteAllBytes(filePath, bytes);
 
-            Destroy(texture);
-
             Debug.Log("Screenshot " + numberPhoto + " saved on " + filePath);
 
             if (numberPhoto < materialToApply.Length)
@@ -87,6 +45,7 @@ public class PhotoManager : MonoBehaviour
                 Texture2D tex = new Texture2D(2, 2);
                 tex.LoadImage(bytes);
                 materialToApply[numberPhoto].mainTexture = tex;
+                images[numberPhoto].texture = tex;
             }
             else
             {
