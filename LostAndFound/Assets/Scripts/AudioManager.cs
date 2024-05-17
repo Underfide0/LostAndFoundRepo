1ea46130c39f80408ee540e17d11a94fd5428237
Underfide0 <ulisesgedo@gmail.com>
2024-05-17 03:17:55 +0200

    Acabado

--- LostAndFound/Assets/Scripts/AudioManager.cs
+++ LostAndFound/Assets/Scripts/AudioManager.cs
@@ -15,7 +15,7 @@ public class AudioManager : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject);
+            
         }
         else
         {
